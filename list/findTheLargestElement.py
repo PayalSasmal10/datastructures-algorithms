1d5811ff36e1088e0cf09bcc32405632e81faf56
payal <sasmalpayal@.com>
2021-12-24 21:26:53 +0530

    largest element count

--- list/findTheLargestElement.py
+++ list/findTheLargestElement.py
@@ -1,5 +1,5 @@
-#Implement a function that takes array of integers as input and returns the largest element.
-# sorted function time complexity is o(n logn)
+# Implement a function that takes array of integers as input and returns the largest element.
+# sorted or sort function time complexity is o(n logn)
 class largestElement:
     def largestelementofaList(arr):
         ls = sorted(arr)
@@ -8,4 +8,20 @@ class largestElement:
 
 le = largestElement
 le.largestelementofaList([-1,-2,-3,-3,8])
+
+# optimize this. O(n) time complexity
+
+def largestEelement(arr):
+    largestNumber = 0
+    for i in arr:
+        largestNumber = max(largestNumber,i)
+    return largestNumber[-1,-2,-3,-3,8]
+
+
+print(largestEelement([-1,-2,-3,-3,8]))
+
+
+
+
+
 
