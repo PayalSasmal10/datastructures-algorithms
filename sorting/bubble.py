94f712c4ed26b12261e7b7f4474cc6b843a45c63
payal <sasmalpayal@.com>
2021-12-27 22:36:29 +0530

    optimized bubble sort

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -1,3 +1,5 @@
+from time import time
+arr_list = [1,2,3,4,5]
 def bubbleSort(arr):
     for i in range(len(arr)):
         for j in range(i+1,len(arr)):
@@ -6,4 +8,24 @@ def bubbleSort(arr):
                 print(arr)
 
 
-bubbleSort([-2,3,4,-1,5,-12,6,1,3])
+start = time()
+bubbleSort(arr_list)
+end = time()-start
+print("Bubble sort ends in "+ str(end) + "seconds")
+
+
+def optimizeBubblesort(arr):
+    swapped = True
+    while(swapped):
+        swapped = False
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                arr[i],arr[i+1] = arr[i+1], arr[i]
+                swapped = True
+        print(arr)
+
+start = time()
+optimizeBubblesort(arr_list)
+end = time()-start
+print("optimized Bubble sort ends in "+ str(end) + "seconds")
+
