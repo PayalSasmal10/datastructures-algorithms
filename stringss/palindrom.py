1f3949c3cb152e79e1c997652fbfc7029beacbe4
payal <sasmalpayal@.com>
2021-12-29 20:40:39 +0530

    palindrom string

---
+++ stringss/palindrom.py
@@ -0,0 +1,16 @@
+# time complexity is O(n)
+palindromString = input()
+
+def palindrome(palimdromString):
+    reversePalindrom = ''
+    for i in palimdromString:
+        reversePalindrom = i + reversePalindrom
+
+    if reversePalindrom == palimdromString:
+        print("It's a palindrome string")
+    else:
+        print("It's not a string")
+
+
+palindrome(palindromString)
+
