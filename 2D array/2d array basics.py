ffad7b64586d361f041f8b245032d6710c2e2f9c
Payal Sasmal <sasmalpayal@gmail.com>
2022-01-10 15:31:03 +0530

    added 2d array concept

---
+++ 2D array/2d array basics.py
@@ -0,0 +1,28 @@
+# create a 2d array
+
+# class twoDimensionalArray:
+arr= []
+# def twoDArray(arr, row, column):
+#     for i in range(row):
+#         for j in range(column):
+#             print(arr[i][j])
+    
+def takeinput():
+    row = int(input())
+    column = int(input())
+    for r in range(row):
+        inner = []
+        for c in range(column):
+            newInput = int(input())
+            inner.append(newInput)
+        arr.append(inner)
+    for k in arr:
+        print(k, sep="\n")
+        
+    # twoDArray(arr, row, column)
+
+
+takeinput()
+
+# object_2dArray = twoDimensionalArray
+# object_2dArray.takeinput()
