3ea0901ab3e9d9ab0506d7136ed70956f85d915f
Payal Sasmal <sasmalpayal@gmail.com>
2022-01-10 11:05:12 +0530

    decimal to binary

--- stringss/decimalTobinary.py
+++ stringss/decimalTobinary.py
@@ -3,10 +3,10 @@
 # output: 1 0 0 0
 
 def decimaltobinary(n):
-    if n>1:
-        n = n//2
-    else:
-        n = n%2
-    return n
+    if n>=1:
+        decimaltobinary(n//2)
+    
+        print(n%2, end=" ")
+    
 
-print(decimaltobinary(8))
+decimaltobinary(8)
