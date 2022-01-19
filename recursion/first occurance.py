5c102464b2570c899796e9572f79d68a7271610b
Payal Sasmal <sasmalpayal@gmail.com>
2022-01-19 21:49:07 +0530

    index of first occurance

---
+++ recursion/first occurance.py
@@ -0,0 +1,11 @@
+# Print index of the first occurance of the value.
+
+arr = [2,5,7,89,2]
+i = 0
+key = 2
+def firstoccurance(arr,i,key):
+    if arr[i] == key:
+        return i
+    return firstoccurance(arr,i+1,key)
+
+print(firstoccurance(arr,i,key))
