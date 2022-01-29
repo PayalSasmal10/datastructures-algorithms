6e1f4596d873d5c5b7352a861864197420806c63
Payal Sasmal <sasmalpayal@gmail.com>
2022-01-29 19:46:28 +0530

    reverseValue updated

--- stringss/reverseValue.py
+++ stringss/reverseValue.py
@@ -1,10 +1,11 @@
 
 # time complexity O(nlogn)
-namereverse = ["payal2", "am1", "i0", "sasmal3"]
+namereverse = "payal2 am1 i0 sasmal3"
 
 sortedValue = sorted(namereverse, key=len)
-sortedValue = " ".join(sortedValue)
+sortedValue = "".join(sortedValue)
 
 removeList = "".join([i for i in sortedValue if not i.isdigit()])
 print(removeList)
+
 
