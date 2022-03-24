c6ad0696cc5909b89af87c8d382d47f68197bd8d
Payal Sasmal <sasmalpayal@gmail.com>
2022-03-24 21:27:00 +0530

    fibonacci added with loop

--- recursion/fibonacci.py
+++ recursion/fibonacci.py
@@ -1,7 +1,25 @@
-# 0 1 1
+# 0 1 1 2 3 5
 def fibonacci(n):
     if n == 0 or n == 1:
         return n
 
     return fibonacci(n-1) + fibonacci(n-2)
-print(fibonacci(3))
+print(fibonacci(5))
+
+# Normal way to do fibonacci 
+
+class A:
+    def fibo(n):
+        f1 = 0
+        f2 = 1
+        test = ""
+        for i in range(1,n):
+            test = f1 + f2
+            f1 = f2
+            f2 = test
+        return test
+
+
+a = A
+print(a.fibo(5))
+            
