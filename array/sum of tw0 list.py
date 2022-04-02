efdfaec0261a525527f12ff641ec869ba6d62aa3
Payal Sasmal <sasmalpayal@gmail.com>
2022-04-02 21:55:48 +0530

    sum of two list and get their MSB and LSB

--- array/sum of tw0 list.py
+++ array/sum of tw0 list.py
@@ -1,22 +1,44 @@
 """
 def main(): 
     print('main') 
+ TestCase1:
     a = [1, 2, 3, 4] 
     b =    [5, 6, 7] 
     # c = [1, 8, 0, 1]
+TEst case2:
+    a = [1, 2, 3, 4]     
+    b = [5, 6, 7,9]
+    #c =[6,9,1,3]
+
+Test case3:
+    a = [2, 3, 4]     
+    b = [5, 6, 7,9]
+    #c = [5,9,1,3]
 
 """
 
-a = [1, 2, 3, 4] 
-b =    [5, 6, 7] 
+a = [2, 3, 4]     
+b = [5, 6, 7, 9]
 sum = new_sum = 0
 new_list = []
-i = len(a)-1
+i = max(len(a)-1, len(b) -1)
 while(i>=0):
-    if i >=1:
-        sum = a[i] + b[i-1] + int(new_sum)
-    else:
-        sum = a[i]
+    if len(b) < len(a):
+        if i >=1:
+            sum = a[i] + b[i-1] + int(new_sum)
+        else:
+            sum = a[i]
+    elif len(b) > len(a):
+        if i>= 1:
+            sum = a[i-1] + b[i] + int(new_sum)
+        else:
+            sum = b[i]
+    elif len(a) == len(b):
+        if i>= 1:
+            sum = a[i] + b[i] + int(new_sum)
+        else:
+            sum = a[i] + b[i]
+        
     if sum >= 10:
         str_sum = str(sum)
         new_sum = str_sum[0]
