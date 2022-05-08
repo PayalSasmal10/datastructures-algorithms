903137270ea4735e7a141ad0e2f587015c2043d1
Payal Sasmal <sasmalpayal@gmail.com>
2022-05-08 11:50:41 +0530

    optimized way using stack

--- stack/nearest greater to right.py
+++ stack/nearest greater to right.py
@@ -3,6 +3,9 @@
 i/p: [1, 3, 0, 0, 1, 2, 4]
 o/p: [3, 4, 1, 1, 2, 4, -1]
 """
+from inspect import stack
+
+
 arr = [1, 3, 0, 0, 1, 2, 4]
 class GreaterRight:
     new_list = []
@@ -20,3 +23,30 @@ class GreaterRight:
 
 g = GreaterRight()
 print(g.nearestLargest(arr))
+
+print("................................................")
+# OPtimize solution 
+class GreaterRightOptimize:
+    stack = []
+    output = []
+    def nearestLargest(self, arr):
+        i = len(arr) - 1
+        while(i>=0):
+            if not self.stack:
+                self.output.append(-1)
+            elif self.stack[-1] > arr[i]:
+                self.output.append(self.stack[-1])
+            elif self.stack[-1] <= arr[i]:
+                self.stack.pop()
+                continue            
+            self.stack.append(arr[i])
+            i -= 1
+
+        return self.output[::-1]
+
+
+
+g = GreaterRightOptimize()
+print(g.nearestLargest(arr))
+
+
