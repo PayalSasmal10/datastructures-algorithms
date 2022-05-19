8b73ad0e0a6c9bcb860183615ebbda510549fb1f
Payal Sasmal <sasmalpayal@gmail.com>
2022-05-19 21:32:31 +0530

    permutation added

--- recursion/coding identification/input_output_method/permutation.py
+++ recursion/coding identification/input_output_method/permutation.py
@@ -1,6 +1,23 @@
 # Find out all permutation
 
-strr = "abc"
-
+strr = "ABC"
+print(strr[2:])
+# output: ABC, ACB, BCA, BAC, CAB, CBA
 class A:
-    pass
+    def permutation(self, strr):
+        output = ""
+        self.solve(strr,output)
+
+    def solve(self, strr, output):
+        if len(strr) == 0:
+            print(output)
+            return
+        for i in range(len(strr)):
+            single_ch = strr[i]
+            output1 = strr[0:i]
+            output2 = strr[i+1:]
+            remaining_str = output1 + output2
+            self.solve(remaining_str, single_ch+output)
+
+a = A()
+a.permutation(strr)
